c48e2e45b81b464ea3ea9a3b448b735813b19572
Pavel Palancica <pavel.palancica@gmail.com>
2018-09-08 22:16:01 +0300

    Added password reset functionality

--- elearn/settings.py
+++ elearn/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 # '/' is hardcoded, but will have the same effect in this case
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
