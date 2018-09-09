cfa61e93ca3a5f2fcb5a24d2a55fd2ad16524448
Pavel Palancica <pavel.palancica@gmail.com>
2018-09-09 09:10:20 +0300

    Added Course view and model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    summary = models.CharField(max_length=255)
+    technology = models.CharField(max_length=100)
+    logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.course_title + ' - ' + self.author
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.lesson_title
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Course
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# https://docs.djangoproject.com/en/2.1/topics/auth/default/#the-loginrequired-mixin
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    # login_url = 'login'
+    # redirect_field_name = 'redirect_to'
+
+    template_name = 'courses/index.html'
+
+    # the default is object_list, but we want to use all_courses inside the views.py file
+    context_object_name = 'all_courses'
+
+    def get_queryset(self):
+        return Course.objects.all()
